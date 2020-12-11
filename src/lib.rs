b6ddd3092e320ad68ddbd424fc697a2e452484bf
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2020-12-11 08:32:30 -0500

    Day 10

--- src/lib.rs
+++ src/lib.rs
@@ -204,5 +204,25 @@ mod tests {
 				);
 			}
 		}
+
+		#[cfg(test)]
+		mod day_10 {
+			use crate::tests::read_input;
+			use crate::year_2020::day_10 as day;
+			const DAY_NUM: u8 = 10;
+
+			#[test]
+			fn part_01() {
+				assert_eq!(day::part_01(&read_input(super::YEAR_NUM, DAY_NUM)), 1914);
+			}
+
+			#[test]
+			fn part_02() {
+				assert_eq!(
+					day::part_02(&read_input(super::YEAR_NUM, DAY_NUM)),
+					9256148959232
+				);
+			}
+		}
 	}
 }
---
+++ src/year_2020/day_10.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+pub fn part_01(input: &String) -> u64 {
+	let mut adapters = input
+		.lines()
+		.map(|l| l.parse::<u64>().unwrap())
+		.collect::<Vec<u64>>();
+	adapters.sort();
+	let mut differences = [0u64; 3];
+	for i in 0..adapters.len() {
+		let difference = if i == 0 {
+			adapters[i]
+		} else {
+			adapters[i] - adapters[i - 1]
+		};
+		differences[difference as usize - 1] += 1;
+	}
+	differences[0] * (differences[2] + 1)
+}
+
+pub fn part_02(input: &String) -> u64 {
+	let mut adapters = input
+		.lines()
+		.map(|l| l.parse::<u64>().unwrap())
+		.collect::<Vec<u64>>();
+	adapters.push(0);
+	adapters.push(adapters.iter().max().unwrap() + 3);
+	adapters.sort_by(|a, b| b.cmp(a));
+	let mut options = HashMap::new();
+	options.insert(adapters[0], 1);
+	for a in &adapters[1..] {
+		options.insert(
+			*a,
+			(1..=3).fold(0, |n, i| {
+				if options.contains_key(&(a + i)) {
+					n + options[&(a + i)]
+				} else {
+					n
+				}
+			}),
+		);
+	}
+	options[&0]
+}
