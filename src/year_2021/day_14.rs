fee0c0984d291ad08b23b926d8bd2364355adf7b
Nicholas Omer Chiasson <nicholasomerchiasson@gmail.com>
2022-12-29 16:01:09 -0500

    Removing unstarted tests

--- src/year_2021/day_14.rs
+++ src/year_2021/day_14.rs
@@ -92,11 +92,11 @@ mod tests {
 	fn part_02() {
 		assert_eq!(
 			super::part_02(&read_resource(relative_input_path(&format!("{}::practice_01", INPUT_PATH)))),
-			1
+			2188189693529
 		);
-		// assert_eq!(
-		// 	super::part_02(&read_resource(relative_input_path(&format!("{}::final", INPUT_PATH)))),
-		// 	1
-		// );
+		assert_eq!(
+			super::part_02(&read_resource(relative_input_path(&format!("{}::final", INPUT_PATH)))),
+			0
+		);
 	}
 }
